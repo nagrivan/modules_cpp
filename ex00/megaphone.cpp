ed8acaa06a20b6f52697e56c50d44027d1bbbcc9
nagrivan <3007klyukva1996@mail.ru>
2021-11-17 14:38:10 +0300

    Написан ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/15 16:52:16 by nagrivan          #+#    #+#             */
+/*   Updated: 2021/11/17 14:35:56 by nagrivan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+	else
+	{
+		
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		std::cout << "\n";
+	}
+	return (0);
+}
