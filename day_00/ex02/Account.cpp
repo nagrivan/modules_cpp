805d2f03cf71065c76d76391d2541c0da90dee99
nagrivan <3007klyukva1996@mail.ru>
2021-12-15 18:33:43 +0300

    ++

--- day_00/ex02/Account.cpp
+++ day_00/ex02/Account.cpp
@@ -6,11 +6,14 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:51:30 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/12/15 13:59:48 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/12/15 17:27:36 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+#include <iostream>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -27,4 +30,54 @@ int Account::getTotalAmount(void)
 	return (_totalAmount);
 }
 
+int Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
 
+int Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+	this->_totalNbDeposits = initial_deposit;
+
+	this->_accountIndex = this->_nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbWithdrawals = this->_totalNbWithdrawals;
+	this->_nbDeposits = 0;
+	
+	std::clog << "index:" << this->_accountIndex << ";";
+	std::clog << "amount:" << this->_amount << ";";
+	std::clog << "created" << std::endl;
+}
+
+Account::~Account(void)
+{
+	std::clog << "index:" << this->_accountIndex << ";";
+	std::clog << "amount:" << this->_amount << ";";
+	std::clog << "closed" << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{}
+
+int Account::checkAmount(void) const
+{}
+
+void	Account::displayStatus(void) const
+{}
+
+void	Account::_displayTimestamp(void)
+{
+}
